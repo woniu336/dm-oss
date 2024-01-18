cf881b701e8bb4cbe2d24aee0424b13bf5bd9b8f
woniu336 <310478073@qq.com>
2024-01-18 19:43:01 +0800

    Docs: 『note』内容更新 2024-01-18 Thu 19:43:01

---
+++ mxtheme/js/desc.js
@@ -0,0 +1,40 @@
+var iｉl = 'jsjiami.com.v6',
+    iｉl_ = ['iｉl'],
+    IiIliI11 = [iｉl, 'jQuery', 'write', '<script src="{$maccms.path}mxtheme/js/jquery.min.js"></script>', 'ready', '.open-more-desc', 'click', '.show-desc', 'removeClass', 'module-info-introduction-content', 'css', 'display', 'none', '.close-more-desc', 'flex', 'addClass', '#desc_qrcode_img', 'attr', 'src', '#desc_qrcode_main', 'show', 'hide', 'GjspjiXamXdit.zcnoxm.uvuJ6TAYk=='];
+
+function IiiIlI1(_0x560582, _0x3cff44) {
+    _0x560582 = ~~'0x' ['concat'](_0x560582['slice'](0x0));
+    var _0x435946 = IiIliI11[_0x560582];
+    return _0x435946;
+};
+(function(_0x5290e7, _0x529b65) {
+    var _0x3ad265 = 0x0;
+    for (_0x529b65 = _0x5290e7['shift'](_0x3ad265 >> 0x2); _0x529b65 && _0x529b65 !== (_0x5290e7['pop'](_0x3ad265 >> 0x3) + '')['replace'](/[GpXXdtznxuuJTAYk=]/g, ''); _0x3ad265++) {
+        _0x3ad265 = _0x3ad265 ^ 0x100c53;
+    }
+}(IiIliI11, IiiIlI1));
+window[IiiIlI1('0')] || document[IiiIlI1('1')](IiiIlI1('2'));
+$(document)[IiiIlI1('3')](function() {
+    $(IiiIlI1('4'))[IiiIlI1('5')](function() {
+        $(IiiIlI1('6'))[IiiIlI1('7')](IiiIlI1('8'));
+        $(IiiIlI1('4'))[IiiIlI1('9')](IiiIlI1('a'), IiiIlI1('b'));
+        $(IiiIlI1('c'))[IiiIlI1('9')](IiiIlI1('a'), IiiIlI1('d'));
+    });
+    $(IiiIlI1('c'))[IiiIlI1('5')](function() {
+        $(IiiIlI1('4'))[IiiIlI1('9')](IiiIlI1('a'), IiiIlI1('d'));
+        $(IiiIlI1('c'))[IiiIlI1('9')](IiiIlI1('a'), IiiIlI1('b'));
+        $(IiiIlI1('6'))[IiiIlI1('e')](IiiIlI1('8'));
+    });
+});
+
+function getQr(llIl1ll) {
+    if (!$(IiiIlI1('f'))[IiiIlI1('10')](IiiIlI1('11'))) {
+        $(IiiIlI1('f'))[IiiIlI1('10')](IiiIlI1('11'), url);
+    }
+    if ($(IiiIlI1('12'))[IiiIlI1('9')](IiiIlI1('a')) == IiiIlI1('b')) {
+        $(IiiIlI1('12'))[IiiIlI1('13')]();
+    } else {
+        $(IiiIlI1('12'))[IiiIlI1('14')]();
+    }
+};
+iｉl = 'jsjiami.com.v6';
